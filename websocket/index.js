954ea5558741741dbe29d37e033a21d01c8c8bcd
hohy <ghgud94@nexivil.com>
2022-02-23 15:24:09 +0900

    support multiUser Chatroom using webRTC

--- websocket/index.js
+++ websocket/index.js
@@ -30,15 +30,34 @@ class Socket {
 
       socket.on("message", (msg) => {
         const message = JSON.parse(msg.utf8Data);
-        console.log(message);
+        // console.log(message);
         switch (message.type) {
           case "init":
             socket["username"] = message.username;
             socket["userid"] = message.userid;
             socket["roomname"] = false;
+            socket["offer"] = false;
+            socket["answer"] = false;
             if (!clients.has(message.userid)) {
               clients.set(message.userid, socket);
             }
+            break;
+          case "all_users":
+            socket.roomname = message.roomname;
+            let allUsers = [];
+            for (const [key, value] of clients) {
+              if (value.roomname === message.roomname) {
+                allUsers.push({
+                  username: value.username,
+                  userid: value.userid,
+                  roomname: value.roomname
+                })
+              }
+            }
+            socket.send(JSON.stringify({
+              type: "all_users",
+              data: allUsers
+            }));
             break;
           case "createroom":
             console.log("CREATEROOM");
@@ -54,28 +73,20 @@ class Socket {
             console.log("ENTERROOMLIST");
             clients.forEach((client) => client.send(JSON.stringify(roomLists)));
             break;
-          case "join_room":
-            socket.roomname = message.roomname;
-            for (const value of clients.values()) {
-              if (value.username === message.username) continue;
-              if (value.roomname === message.roomname) {
-                value.send(
-                  JSON.stringify({
-                    type: "welcome",
-                    roomname: message.roomname,
-                  })
-                );
-              }
-            }
-            break;
           case "offer":
             for (const value of clients.values()) {
-              if (value.username === message.username) continue;
-              if (value.roomname === message.roomname) {
+              if (
+                value.roomname === message.roomname &&
+                value.userid === message.userid
+              ) {
                 value.send(
                   JSON.stringify({
                     type: "offer",
                     offer: message.offer,
+                    userid: value.userid,
+                    username: value.username,
+                    offerUsername: message.sendUsername,
+                    offerUserid: message.sendUserid,
                     roomname: message.roomname,
                   })
                 );
@@ -84,13 +95,16 @@ class Socket {
             break;
           case "answer":
             for (const value of clients.values()) {
-              if (value.username === message.username) continue;
-              if (value.roomname === message.roomname) {
+              if (value.username === message.sendUsername && value.roomname === message.roomname) {
                 value.send(
                   JSON.stringify({
                     type: "answer",
                     answer: message.answer,
                     roomname: message.roomname,
+                    username: value.username,
+                    userid: message.userid,
+                    sendUserid: message.sendUserid,
+                    sendUsername: message.sendUsername
                   })
                 );
               }
@@ -98,20 +112,32 @@ class Socket {
             break;
           case "ice":
             for (const value of clients.values()) {
-              if (value.username === message.username) continue;
-              if (value.roomname === message.roomname) {
+              if (value.roomname === message.roomname && value.username === message.username) {
                 value.send(
                   JSON.stringify({
                     type: "ice",
                     candidate: message.candidate,
                     roomname: message.roomname,
+                    username: value.username,
+                    userid: value.userid,
+                    sendUsername: message.sendUsername,
+                    sendUserid: message.sendUserid
                   })
                 );
               }
             }
             break;
+            case "exit":
+              console.log("EXIT")
+              clients.get(message.userid).roomname = false;
+              break;
         }
       });
+
+      socket.on("close", () => {
+        console.log("close")
+        clients.delete(socket.userid);
+      })
     });
   }
 }
